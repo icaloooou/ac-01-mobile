9b23a9575db6cdbca66cb84a86bd57daa2dfb5f3
Gustavo Moraes <57626705+Gustavo-dm@users.noreply.github.com>
2021-09-08 08:58:44 -0300

    Update ac01.kt

--- ac01.kt
+++ ac01.kt
@@ -1,6 +1,12 @@
+/*
+Felipe Waddington Pereira Jeronymo - 1904837
+Hires Serva de Maria Menezes - 1904889
+Igor Luan Teles de Souza - 1905365
+Ingrid Cristinne Calou Batista - 1904821
+Rodrigo Augusto Aniceto Alves - 1904668
+Gustavo de Moraes Silva - 1905338
 
-import java.math.BigDecimal
-
+*/
 
 fun main() {
     println(calculaIdade(2021, 2001))
